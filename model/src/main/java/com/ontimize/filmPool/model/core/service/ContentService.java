296eab536e7bf2f6e3394b59aebee4fb75ddcff8
María Paz López <maria.lopez@campusdual.com>
2020-06-12 10:12:59 +0200

    Añadido CRUD en el servicio Content

--- model/src/main/java/com/ontimize/filmPool/model/core/service/ContentService.java
+++ model/src/main/java/com/ontimize/filmPool/model/core/service/ContentService.java
@@ -3,16 +3,15 @@ package com.ontimize.filmPool.model.core.service;
 import com.ontimize.db.EntityResult;
 import com.ontimize.filmPool.api.core.service.IContenidosService;
 import com.ontimize.filmPool.model.core.dao.ContentDao;
+import com.ontimize.filmPool.model.core.dao.StudioDao;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service("ContentService")
 @Lazy
@@ -23,19 +22,36 @@ public class ContentService implements IContenidosService {
     private ContentDao contentDao;
     @Autowired
     private DefaultOntimizeDaoHelper daoHelper;
+    private ContentService contentService;
 
     public EntityResult contentQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
         return this.daoHelper.query(this.contentDao, keyMap, attrList);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public EntityResult contentInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
-        return this.daoHelper.insert(this.contentDao,attrMap);
+        Map<String, Object> nonContentData = removeNonRelatedData(attrMap, ContentDao.CONTENT_NAME,
+                ContentDao.CONTENT_NACIONALITY, ContentDao.CONTENT_POSTER_PATH, ContentDao.CONTENT_PLOT,
+                ContentDao.CONTENT_DURATION, ContentDao.CONTENT_RELEASE_DATE);
+        this.insertNonRelatedData(nonContentData);
+        attrMap.putAll(nonContentData);
+        return this.daoHelper.insert(this.contentDao, attrMap);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public EntityResult contentUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+        Map<String, Object> nonContentData= removeNonRelatedData(attrMap, ContentDao.CONTENT_NAME,
+                ContentDao.CONTENT_NACIONALITY, ContentDao.CONTENT_POSTER_PATH, ContentDao.CONTENT_PLOT,
+                ContentDao.CONTENT_DURATION, ContentDao.CONTENT_RELEASE_DATE);
+        this.insertNonRelatedData(nonContentData);
+        attrMap.putAll(nonContentData);
         return this.daoHelper.update(this.contentDao,attrMap,keyMap);
+    }
+
+    private Map<String, Object> insertNonRelatedData(Map<String, Object> nonContentData) throws OntimizeJEERuntimeException {
+        return nonContentData;
     }
 
     @Override
@@ -43,13 +59,15 @@ public class ContentService implements IContenidosService {
         return this.daoHelper.delete(this.contentDao,keyMap);
     }
 
-    public EntityResult newMovies()
-            throws OntimizeJEERuntimeException {
-        Map<String, Object> keyMap= new HashMap<String, Object>();
-        List<String> attrList = Arrays.asList(contentDao.CONTENT_NAME,contentDao.CONTENT_DURATION,contentDao.CONTENT_ID);
-        return this.daoHelper.query(this.contentDao, keyMap, attrList);
+    private Map<String, Object> removeNonRelatedData(Map<String, Object> attrMap, String... attrToExclude) {
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        for (String attr : attrToExclude) {
+            if (attrMap.containsKey(attr) && attrMap.get(attr) instanceof String) {
+                data.put(attr, attrMap.remove(attr));
+            }
+        }
+        return data;
     }
-
 
 
 }
