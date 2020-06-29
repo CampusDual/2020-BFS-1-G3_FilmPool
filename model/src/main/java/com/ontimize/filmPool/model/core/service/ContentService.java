093a14fbd9253d796985d50e7ee54ab79ed3096a
María Paz López <maria.lopez@campusdual.com>
2020-06-29 16:22:58 +0200

    Solucionando los últimos detalles en el servicio del contenido

--- model/src/main/java/com/ontimize/filmPool/model/core/service/ContentService.java
+++ model/src/main/java/com/ontimize/filmPool/model/core/service/ContentService.java
@@ -30,34 +30,14 @@ public class ContentService implements IContentService {
         return this.daoHelper.query(this.contentDao, keyMap, attrList);
     }
 
-    @Transactional(rollbackFor = Exception.class)
     public EntityResult contentInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
-        Map<String, Object> nonContentData = removeNonRelatedData(attrMap, ContentDao.CONTENT_NAME,
-                ContentDao.CONTENT_NACIONALITY, ContentDao.CONTENT_POSTER_PATH, ContentDao.CONTENT_PLOT,
-                ContentDao.CONTENT_DURATION, ContentDao.CONTENT_RELEASE_DATE);
-        this.insertNonRelatedData(nonContentData);
-        attrMap.putAll(nonContentData);
+
         return this.daoHelper.insert(this.contentDao, attrMap);
     }
 
-    @Transactional(rollbackFor = Exception.class)
     public EntityResult contentUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
-        Map<String, Object> nonContentData = removeNonRelatedData(attrMap, ContentDao.CONTENT_NAME,
-                ContentDao.CONTENT_NACIONALITY, ContentDao.CONTENT_POSTER_PATH, ContentDao.CONTENT_PLOT,
-                ContentDao.CONTENT_DURATION, ContentDao.CONTENT_RELEASE_DATE);
-        this.insertNonRelatedData(nonContentData);
-        attrMap.putAll(nonContentData);
-        return this.daoHelper.update(this.contentDao, attrMap, keyMap);
-    }
 
-    private Map<String, Object> removeNonRelatedData(Map<String, Object> attrMap, String... attrToExclude) {
-        HashMap<String, Object> data = new HashMap<String, Object>();
-        for (String attr : attrToExclude) {
-            if (attrMap.containsKey(attr) && attrMap.get(attr) instanceof String) {
-                data.put(attr, attrMap.remove(attr));
-            }
-        }
-        return data;
+        return this.daoHelper.update(this.contentDao, attrMap, keyMap);
     }
 
     public EntityResult contentDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
@@ -74,38 +54,6 @@ public class ContentService implements IContentService {
             throws OntimizeJEERuntimeException {
         Map<String, Object> keyMap = new HashMap<String, Object>();
         return this.daoHelper.query(this.contentDao, keyMap, columns, "latestShows");
-    }
-
-    private void insertNonRelatedData(Map<String, Object> nonContentData) throws OntimizeJEERuntimeException {
-        for (java.util.Map.Entry<String, Object> entry : nonContentData.entrySet()) {
-            Map<String, Object> data = new HashMap<String, Object>();
-            List<String> attr = new ArrayList<String>();
-            EntityResult toret, query;
-            switch (entry.getKey()) {
-                case ContentDao.STUDIO_ID:
-                    data.put((String) StudioDao.STUDIO_NAME, entry.getValue());
-                    attr.add((String) StudioDao.STUDIO_ID);
-                    query = this.contentService.studioQuery(data, attr);
-                    if (query.calculateRecordNumber() > 0) {
-                        entry.setValue((Object) query.getRecordValues(0).get(StudioDao.STUDIO_ID));
-                    } else {
-                        toret = this.contentService.studioInsert(data);
-                        entry.setValue((Object) toret.get(StudioDao.STUDIO_ID));
-                    }
-                    break;
-                case ContentDao.GENRE_ID:
-                    data.put((String) StudioDao.STUDIO_NAME, entry.getValue());
-                    attr.add((String) StudioDao.STUDIO_ID);
-                    query = this.contentService.genresQuery(data, attr);
-                    if (query.calculateRecordNumber() > 0) {
-                        entry.setValue((Object) query.getRecordValues(0).get(StudioDao.STUDIO_ID));
-                    } else {
-                        toret = this.contentService.genresInsert(data);
-                        entry.setValue((Object) toret.get(StudioDao.STUDIO_ID));
-                    }
-                    break;
-            }
-        }
     }
 
 }
