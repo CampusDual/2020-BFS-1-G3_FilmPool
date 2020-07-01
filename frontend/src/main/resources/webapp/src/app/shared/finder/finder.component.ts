07511948d6412a07ca9ff29134c80d5469cb31c6
JoseCabezasPok <jose.cabezas@campusdual.com>
2020-07-01 17:42:44 +0200

    feature buscardor funcionando

--- frontend/src/main/resources/webapp/src/app/shared/finder/finder.component.ts
+++ frontend/src/main/resources/webapp/src/app/shared/finder/finder.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from "../../services/data.service";
+import {Observable} from "rxjs";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-finder',
   templateUrl: './finder.component.html',
-  styleUrls: ['./finder.component.scss']
+  styleUrls: ['./finder.component.scss'],
+  providers: [DataService]
 })
 export class FinderComponent implements OnInit {
+  public data$ = [];
+  public keyword = "content_name";
+  public notFound = "No se encuentra"
+  constructor(private dataService :DataService) {
 
-  constructor() { }
+  }
 
+  getData(){
+     this.dataService.getAllContent().subscribe(response =>{
+      response["data"].forEach(content=>{
+        this.data$.push(content);
+      })
+     });
+  }
   ngOnInit() {
+      this.getData();
+  }
+  onSelected(item){
+      //Cuando se selecciona un titulo de la lista este evento redirigira a componente
+      //detalle de la misma, por ahora imprime nombre en consola
+    console.log(item.content_name);
   }
 
 }
--- frontend/src/main/resources/webapp/src/app/shared/shared.module.ts
+++ frontend/src/main/resources/webapp/src/app/shared/shared.module.ts
@@ -7,12 +7,14 @@ import { FinderComponent } from './finder/finder.component';
 import { CastComponent } from './cast/cast.component';
 import { RatingComponent } from './rating/rating.component';
 import {RouterModule} from "@angular/router";
+import {AutocompleteLibModule} from "angular-ng-autocomplete";
 
 
 @NgModule({
     imports: [
         OntimizeWebModule,
-        RouterModule
+        RouterModule,
+        AutocompleteLibModule
     ],
   declarations: [
   HeaderComponent,
